3e8241c6d5178b401e81f900e4d66c334badcfc3
JesseWu <wjxdem@gmail.com>
2019-03-30 18:16:49 +0800

    add swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   },
   name: 'Home'
 }
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="warpper">
+    <swiper :options="swiperOption">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl" />
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '00001',
+        imgUrl: require('@/assets/images/db.jpg')
+      },
+      {
+        id: '00002',
+        imgUrl: require('@/assets/images/4d.jpg')
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.warpper >>> .swiper-pagination-bullet-active {
+  background: #fff;
+}
+.warpper
+  width: 100%
+  height: 0
+  overflow hidden
+  padding-bottom:46%
+  .swiper-img
+    width: 100%
+</style>
