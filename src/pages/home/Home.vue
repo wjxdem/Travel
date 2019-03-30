fe657c36c6ba7489dce96228ca3e183ee5140bf8
JesseWu <wjxdem@gmail.com>
2019-03-30 12:53:10 +0800

    first add

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <div>Hell word</div>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
+<style scoped>
 
-<style>
 </style>
