347cc1bc24d832b635240f3e724163f4e8218cda
JesseWu <wjxdem@gmail.com>
2019-04-03 23:20:35 +0800

    add city

---
+++ src/pages/city/City.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <City-header></City-header>
+  </div>
+</template>
+<script>
+import CityHeader from './components/Header'
+export default {
+  components: {
+    CityHeader
+  },
+  name: 'City'
+}
+</script>
+<style scoped>
+
+</style>
--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,8 +1,10 @@
-<template>
+ <template>
   <div class="icons">
     <div class="icon">
-      <img src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png"/>
-      <p></p>
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png"/>
+      </div>
+      <p class="icon-desc">热门景点</p>
     </div>
   </div>
 </template>
@@ -14,21 +16,36 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/varibles.styl';
   .icons
     overflow: hidden
     height: 0
-    padding-bottom:50%
+    padding-bottom: 50%
     .icon
-      width:25%
+      position: relative
+      overflow: hidden
+      width: 25%
       float: left
-      padding-bottom:25%
+      padding-bottom: 25%
       .icon-img
         position: absolute
         top: 0
         left: 0
         right: 0
         bottom: 0.44rem
+        box-sizing: border-box
+        padding:. 1rem
         .icon-img-content
           height: 100%
-
+          display: block
+          margin: 0 atuo
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        botton: 0
+        height: .44rem
+        line-height: .44rem
+        color: $darkTextColor
+        text-align: center
 </style>
